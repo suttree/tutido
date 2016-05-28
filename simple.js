f63a3521d760891fd3dbf02639eaa418a397f9b1
suttree <duncan@suttree.com>
2016-05-28 10:27:46 +0100

    work in progress

--- simple.js
+++ simple.js
@@ -1,9 +1,12 @@
+// TODO hide access token
+//
 // create mp3s with link and picture in the metadata (for the iPlayer archives?)
 
 var fs = require('fs'),
     Ivona = require('ivona-node'),
     read = require("node-readability"),
     sanitizer = require("sanitizer"),
+    request = require('request'),
     Entities = require('html-entities').AllHtmlEntities;
 
 var ivona = new Ivona({
@@ -17,13 +20,16 @@ var entities = new Entities();
 function scraper(url, callback) {
     read(url, function(err, doc) {
         if (err) {
+          console.log(err);
             throw err;
         }
 
         var obj = {
             "url": url,
             "title": doc.title.trim(),
-            "contents": stripHTML(doc.content || "")
+            "contents": stripHTML(doc.content || ""),
+            "test": doc.content,
+            "html": doc.html,
         };
         callback(obj);
     });
@@ -48,39 +54,88 @@ function stripHTML(html) {
 }
 
 var args = process.argv.slice(2);
-scraper(args[0], function (data) {
-  var article;
-  var concat_list = 'concat:';
 
-  article = data.title + '\n\n' + data.contents
-  //console.log("# %s #\n\n%s\n\n---", data.title, data.contents);
+url = "http://readability.com/api/content/v1/parser?url=" + args[0] + "&token=047be2416589ced8d31b1f929286c2b9087eee7b"
+console.log(url);
+request(url, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var article;
+    var concat_list = 'concat:';
 
-  article.split('\n').forEach(function (line, i) {
-    ivona.createVoice(line, {
-        body: {
-            voice: {
-                name: 'Emma',
-                language: 'en-GB',
-                gender: 'Female'
-            }
-        }
-    }).pipe(fs.createWriteStream('tmp/text-' + i + '.mp3'));
-    concat_list += 'tmp/text-' + i + '.mp3|';
-  });
+    body = JSON.parse(body);
+    article = body['title'] + '\n' + body['content'];
+    article = stripHTML(article);
+    //console.log(article);
 
-  //const exec = require('child_process').exec;
-  //console.log(concat_list);
-  var cmd = 'ffmpeg -i "' + concat_list.slice(0, -1) + '" -c copy content/article.mp3';
-  console.log(cmd);
+    article.split('\n').forEach(function (line, i) {
+      console.log(line);
+      ivona.createVoice(line, {
+          body: {
+              voice: {
+                  name: 'Emma',
+                  language: 'en-GB',
+                  gender: 'Female'
+              }   
+          }   
+      }).pipe(fs.createWriteStream('tmp/text-' + i + '.mp3'));
+      concat_list += 'tmp/text-' + i + '.mp3|';
 
-  require('shelljs/global');
-  //exec(cmd, {silent:true}).output;
+      //const exec = require('child_process').exec;
+      //console.log(concat_list);
+      var cmd = 'ffmpeg -i "' + concat_list.slice(0, -1) + '" -c copy content/article.mp3';
+      console.log(cmd);
 
-  exec(cmd, function(err, out, code) {
-    if (err instanceof Error)
-      throw err;
-    process.stderr.write(err);
-    process.stdout.write(out);
-    process.exit(code);
-  });
+      require('shelljs/global');
+      //exec(cmd, {silent:true}).output;
+
+      //exec(cmd, function(err, out, code) {
+      //  if (err instanceof Error)
+      //    throw err;
+      //  process.stderr.write(err);
+      //  process.stdout.write(out);
+      //  process.exit(code);
+      //});
+    });
+  } else {
+    console.log("ERROR");
+  }
 });
+//scraper(args[0], function (data) {
+//  var article;
+//  var concat_list = 'concat:';
+//
+//  article = data.title + '\n\n' + data.contents
+//  console.log(data.test);
+//  console.log(data.contents);
+//  console.log(data.html);
+//  //console.log("# %s #\n\n%s\n\n---", data.title, data.contents);
+//
+//  article.split('\n').forEach(function (line, i) {
+//    ivona.createVoice(line, {
+//        body: {
+//            voice: {
+//                name: 'Emma',
+//                language: 'en-GB',
+//                gender: 'Female'
+//            }
+//        }
+//    }).pipe(fs.createWriteStream('tmp/text-' + i + '.mp3'));
+//    concat_list += 'tmp/text-' + i + '.mp3|';
+//  });
+//
+//  //const exec = require('child_process').exec;
+//  //console.log(concat_list);
+//  var cmd = 'ffmpeg -i "' + concat_list.slice(0, -1) + '" -c copy content/article.mp3';
+//  console.log(cmd);
+//
+//  require('shelljs/global');
+//  //exec(cmd, {silent:true}).output;
+//
+//  exec(cmd, function(err, out, code) {
+//    if (err instanceof Error)
+//      throw err;
+//    process.stderr.write(err);
+//    process.stdout.write(out);
+//    process.exit(code);
+//  });
+//});
