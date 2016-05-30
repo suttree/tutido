f51ba7e2dee46617de37809835f3cd3cf508875b
suttree <duncan@suttree.com>
2016-05-30 18:40:39 +0100

    wtf

--- simple.js
+++ simple.js
@@ -14,6 +14,16 @@ var ivona = new Ivona({
     secretKey: 'l5tEgLDQzeR85ZuH8/VMvS1Px7Noltuac6ZDjQwQ'
 });
 
+ivona.createVoice("hello duncan", {
+    body: {
+        voice: {
+            name: 'Emma',
+            language: 'en-GB',
+            gender: 'Female'
+        }
+    }
+}).pipe(fs.createWriteStream('tmp/duncan.mp3'));
+
 var entities = new Entities();
 
 // from http://nodeexamples.com/2012/09/27/scraping-a-pages-content-using-the-node-readability-module-and-node-js/
@@ -57,91 +67,67 @@ var args = process.argv.slice(2);
 
 url = "http://readability.com/api/content/v1/parser?url=" + args[0] + "&token=047be2416589ced8d31b1f929286c2b9087eee7b"
 console.log(url);
+
+var article;
+var concat_list = 'concat:';
 request(url, function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    var article;
-    var concat_list = 'concat:';
+    //var article;
+    //var concat_list = 'concat:';
 
     body = JSON.parse(body);
     article = body['title'] + '\n' + body['content'];
     article = stripHTML(article);
-    //console.log(article);
+    createRecording(article);
 
-    ivona.listVoices().on('complete', function(voices) {
-      console.log(voices);
-      console.log(11);
-   });
-    //article.replace(/([.?!])\s*(?=[A-Z])/g, "$1|").split("|").forEach(function (line, i) {
-    article.split("\n").forEach(function (line, i) {
-      console.log(line);
-      ivona.createVoice(line, {
-          body: {
-              voice: {
-                  name: 'Emma',
-                  language: 'en-GB',
-                  gender: 'Female'
-              }
-          }
-      }).pipe(fs.createWriteStream('tmp/text-' + i + '.mp3'));
-      concat_list += 'tmp/text-' + i + '.mp3|';
-    });
-
-    //const exec = require('child_process').exec;
-    //console.log(concat_list);
-    var cmd = 'ffmpeg -i "' + concat_list.slice(0, -1) + '" -c copy content/article.mp3';
-    console.log(cmd);
-
-    require('shelljs/global');
-    //exec(cmd, {silent:true}).output;
-
-    exec(cmd, function(err, out, code) {
-      if (err instanceof Error)
-        throw err;
-      process.stderr.write(err);
-      process.stdout.write(out);
-      process.exit(code);
-    });
+    //ivona.listVoices().on('complete', function(voices) {
+    //  console.log(voices);
+    //  console.log(11);
+    //});
   } else {
     console.log("ERROR");
   }
-
 });
-//scraper(args[0], function (data) {
-//  var article;
-//  var concat_list = 'concat:';
-//
-//  article = data.title + '\n\n' + data.contents
-//  console.log(data.test);
-//  console.log(data.contents);
-//  console.log(data.html);
-//  //console.log("# %s #\n\n%s\n\n---", data.title, data.contents);
-//
-//  article.split('\n').forEach(function (line, i) {
-//    ivona.createVoice(line, {
-//        body: {
-//            voice: {
-//                name: 'Emma',
-//                language: 'en-GB',
-//                gender: 'Female'
-//            }
-//        }
-//    }).pipe(fs.createWriteStream('tmp/text-' + i + '.mp3'));
-//    concat_list += 'tmp/text-' + i + '.mp3|';
-//  });
-//
-//  //const exec = require('child_process').exec;
-//  //console.log(concat_list);
-//  var cmd = 'ffmpeg -i "' + concat_list.slice(0, -1) + '" -c copy content/article.mp3';
-//  console.log(cmd);
-//
-//  require('shelljs/global');
-//  //exec(cmd, {silent:true}).output;
-//
-//  exec(cmd, function(err, out, code) {
-//    if (err instanceof Error)
-//      throw err;
-//    process.stderr.write(err);
-//    process.stdout.write(out);
-//    process.exit(code);
-//  });
-//});
+
+function createRecording(article) {
+  article.split("\n").forEach(function (line, i) {
+    //console.log(line);
+    //console.log('-----');
+    ivona.createVoice(line, {
+        body: {
+            voice: {
+                name: 'Emma',
+                language: 'en-GB',
+                gender: 'Female'
+            }
+        }
+    }).pipe(fs.createWriteStream('tmp/text-' + i + '.mp3'));
+    concat_list += 'tmp/text-' + i + '.mp3|';
+
+    ivona.createVoice('Google enlists artists to make bots feel like friends', {
+        body: {
+            voice: {
+                name: 'Emma',
+                language: 'en-GB',
+                gender: 'Female'
+            }
+        }
+    }).pipe(fs.createWriteStream('tmp/duncan.mp3'));
+  });
+
+  //const exec = require('child_process').exec;
+  //console.log(concat_list);
+  var cmd = 'ffmpeg -i "' + concat_list.slice(0, -1) + '" -c copy content/article.mp3';
+  console.log(cmd);
+
+  require('shelljs/global');
+  //exec(cmd, {silent:true}).output;
+
+  exec(cmd, function(err, out, code) {
+    if (err instanceof Error)
+      throw err;
+    process.stderr.write(err);
+    process.stdout.write(out);
+    process.exit(code);
+  });
+}
